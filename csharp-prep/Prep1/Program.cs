a3e2f9e6f728632ac655556a20ae48c6384137b0
Lars Phenix <lars44627@gmail.com>
2025-01-10 15:15:51 -0700

    week1 prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("Your first name?:");
+        string name = Console.ReadLine();
+        Console.Write("Your last name?:");
+        string name2 = Console.ReadLine();
+        Console.WriteLine($"your name is {name2}, {name} {name2}");
     }
 }
