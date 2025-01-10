a863eb02e3fc376c88b438ddfd8853c7f028752d
Lars Phenix <lars44627@gmail.com>
2025-01-10 15:57:13 -0700

    week 1 prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your number grade for the course?:");
+        string grade = Console.ReadLine();
+        int gradeN = int.Parse(grade);
+        string letter = "";
+
+
+        if (gradeN >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradeN >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradeN >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradeN >= 60)
+        {
+            letter = "D";
+        }
+        else if (gradeN < 60)
+        {
+            letter = "F";
+        }
+
+        if (gradeN >= 70)
+        {
+            Console.WriteLine(" Congratulations! you passed");
+        }
+        else if (gradeN < 70)
+        {
+            Console.WriteLine("sorry, but you can get it next time!");
+        }
+        Console.WriteLine($"your letter grade: {letter}");
     }
 }
