cbc40eb419f6da6cbea2313cc98a1d5156c3af43
Lars Phenix <lars44627@gmail.com>
2025-01-15 12:29:10 -0700

    loops update

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,35 @@
 using System;
+using System.Formats.Asn1;
+using System.Numerics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("Try and guess the magic number");
+        Random randomGenerator = new Random();
+        int magicNum = randomGenerator.Next(1, 101);
+
+        int guess;
+
+    do
+    {
+        Console.WriteLine("guess the magic number:");
+        guess = int.Parse(Console.ReadLine());
+
+        if (guess > magicNum)
+        {
+Console.WriteLine("lower");
+        }
+        else if (guess < magicNum)
+        {
+Console.WriteLine("higher");
+        }
+
+    }
+    while (guess != magicNum);
+
+
+Console.WriteLine("Congrats, you guessed the number!");
     }
 }
