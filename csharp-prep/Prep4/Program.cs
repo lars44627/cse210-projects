d385607ecd080152633cbe6aff90e0d9f63ed692
Lars Phenix <lars44627@gmail.com>
2025-01-16 16:45:07 -0700

    prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,49 @@
 using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+
+
+
+
+        int num = 0;
+        List<int> numbers = new List<int>();
+
+        do{
+        Console.WriteLine("Enter a number to add it to the list, enter 0 to end");
+        string input = Console.ReadLine();
+        num = int.Parse(input);
+        if (num != 0)
+        {
+            numbers.Add(num);
+        }
+
+        }while (num != 0);
+
+
+        int highest = 0;
+        int total = 0;
+        foreach(int n in numbers)
+        {
+            total += n;
+            if(n > highest)
+            {
+            highest = n;
+            }
+        }
+        Console.WriteLine($"Sum Total: {total}");
+
+        int count = numbers.Count;
+        double avg = numbers.Average();
+        Console.WriteLine($"Average: {avg}");
+
+        Console.WriteLine($"Highest Number: {highest}");
+
+
     }
 }
