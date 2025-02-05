a728520736d9631ea98ea3c71338ee1df71c8eeb
Lars Phenix <lars44627@gmail.com>
2025-02-05 15:40:13 -0700

    setter changes and verify

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -7,5 +7,16 @@ class Program
         Fraction def = new Fraction();
         Fraction one = new Fraction(5);
         Fraction two = new Fraction(3, 4);
+
+        def.Top = 2;
+        def.Bottom = 5;
+        one.Top = 7;
+        one.Bottom = 9;
+        two.Top = 8;
+        two.Bottom = 10;
+
+        Console.WriteLine($"Updated Default Fraction: {def.Top}/{def.Bottom}");
+        Console.WriteLine($"Updated Fraction One: {one.Top}/{one.Bottom}");
+        Console.WriteLine($"Updated Fraction Two: {two.Top}/{two.Bottom}");
     }
 }
--- prepare/Learning03/fraction.cs
+++ prepare/Learning03/fraction.cs
@@ -1,25 +1,34 @@
 
 using System.Globalization;
 
-public class fraction
+public class Fraction
 {
 private int _top = 0; 
 private int _bottom = 0;
 
-    public fraction()
+    public Fraction()
     {
         _top = 1;
         _bottom = 1;
     }
-    public fraction(int top)
+    public Fraction(int top)
     {
         _top = top;
         _bottom = 1;
     }
-    public fraction(int top, int bottom)
+    public Fraction(int top, int bottom)
     {
         _top = top;
         _bottom = bottom;
     }
-
+     public int Top
+    {
+        get { return _top; }
+        set { _top = value; }
+    }
+    public int Bottom
+    {
+        get { return _bottom; }
+        set { _bottom = value; }
+    }
 }
