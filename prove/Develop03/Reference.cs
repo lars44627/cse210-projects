c8f4b0a7a814e102b6f75cf559100cda55aa617c
Lars Phenix <lars44627@gmail.com>
2025-02-12 11:51:26 -0700

    part1(ref with constructs)

---
+++ prove/Develop03/Reference.cs
@@ -0,0 +1,27 @@
+using System;
+
+
+public class Reference
+{
+    private string _book;
+    private int _chapter;
+    private int _verseStart;
+    private int _verseEnd;
+
+    public Reference(string book, int chapter, int verse)
+    {
+        _book = book;
+        _chapter = chapter;
+        _verseStart = verse;
+        _verseEnd = verse;
+
+    }
+    public Reference(string book, int chapter, int verseStart, int verseEnd)
+    {
+        _book = book;
+        _chapter = chapter;
+        _verseStart = verseStart;
+        _verseEnd = verseEnd;
+
+    }
+}
