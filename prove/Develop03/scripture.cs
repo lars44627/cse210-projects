99551a0d571036f3611830dcc4d3decd7c87ba26
Lars Phenix <lars44627@gmail.com>
2025-02-13 17:23:29 -0700

    more

--- prove/Develop03/scripture.cs
+++ prove/Develop03/scripture.cs
@@ -1,44 +1,72 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 
 public class Scripture
 {
     private Reference _reference;
     private List<Word> _words;
-    private Random _random;
+    private Random _random = new Random();
 
     public Scripture(Reference reference, string text)
     {
         _reference = reference;
-        _words = text.Split(' ').Select(word => new Word(word)).ToList();
-        _random = new Random();
+        _words = new List<Word>();
+        
+        string[] wordsArray = text.Split(' ');
+        foreach (string word in wordsArray)
+        {
+            _words.Add(new Word(word));
+        }
     }
 
     public void Display()
     {
         Console.Clear();
         Console.WriteLine(_reference.GetReference());
-        Console.WriteLine(string.Join(" ", _words.Select(word => word.GetDisplayText())));
+
+        string displayText = "";
+        foreach (Word word in _words)
+        {
+            displayText += word.GetDisplayText() + " ";
+        }
+
+        Console.WriteLine(displayText.Trim());
     }
 
     public void HideWords()
     {
         int wordsToHide = 3;
         int hiddenCount = 0;
-        List<Word> visibleWords = _words.Where(word => !word.IsHidden()).ToList();
-
-        while (hiddenCount < wordsToHide && visibleWords.Count > 0)
+        List<int> visibleIndexes = new List<int>();
+        
+        for (int i = 0; i < _words.Count; i++)
         {
-            int index = _random.Next(visibleWords.Count);
-            visibleWords[index].Hide();
-            visibleWords.RemoveAt(index);
+            if (!_words[i].IsHidden())
+            {
+                visibleIndexes.Add(i);
+            }
+        }
+
+        while (hiddenCount < wordsToHide && visibleIndexes.Count > 0)
+        {
+            int randomIndex = _random.Next(visibleIndexes.Count);
+            int wordIndex = visibleIndexes[randomIndex];
+
+            _words[wordIndex].Hide();
+            visibleIndexes.RemoveAt(randomIndex);
             hiddenCount++;
         }
     }
 
     public bool IsFullyHidden()
     {
-        return _words.All(word => word.IsHidden());
+        foreach (Word word in _words)
+        {
+            if (!word.IsHidden())
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
