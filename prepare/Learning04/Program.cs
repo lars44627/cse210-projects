694cebfc51ff816a4c7b5c35770f4f4311d1b3f9
Lars Phenix <lars44627@gmail.com>
2025-02-19 17:05:49 -0700

    4

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -6,5 +6,10 @@ class Program
     {
         Assignment assignment = new Assignment("Samuel Bennett", "Division");
         Console.WriteLine(assignment.GetSummary());
+
+        MathAssignment mathAssignment = new MathAssignment("Samuel Bennett", "Division", "5.1", "1-20");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
     }
 }
