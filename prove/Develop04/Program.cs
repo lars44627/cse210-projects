51d888ffb4200ad149587593a6de575722219b99
Lars Phenix <lars44627@gmail.com>
2025-02-26 11:59:51 -0700

    prog 1

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -2,8 +2,25 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    public Program()
     {
-        Console.WriteLine("Hello Develop04 World!");
+        Menu();
+        string choice = Console.ReadLine();
+        
+        if (choice == "4") return;
+        if (choice == "1") new BreathingActivity().Start();
+        else if (choice == "2") new ReflectionActivity().Start();
+        else if (choice == "3") new ListingActivity().Start();
+        else Console.WriteLine("Invalid choice, exiting program.");
+    }
+
+    public void Menu()
+    {
+        Console.WriteLine("Choose an activity:");
+        Console.WriteLine("1. Breathing Activity");
+        Console.WriteLine("2. Reflection Activity");
+        Console.WriteLine("3. Listing Activity");
+        Console.WriteLine("4. Exit");
+        Console.Write("Enter your choice: ");
     }
 }
