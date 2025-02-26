21f40ae0fd32f4777de79a6d9a853cf251c9cd26
Lars Phenix <lars44627@gmail.com>
2025-02-26 12:28:39 -0700

    lisr1

---
+++ prove/Develop04/ListingActivity.cs
@@ -0,0 +1,17 @@
+class ListingActivity : Activity
+{
+    string[] _prompts = { "List people you appreciate.", "List your strengths." };
+    protected string GetDescription() { return "List as many positive things as you can."; }
+    protected void Run()
+    {
+        Console.WriteLine(_prompts[new Random().Next(_prompts.Length)]);
+        Pause(3);
+        int count = 0;
+        Console.WriteLine("Start listing items (press enter after each):");
+        for (int elapsed = 0; elapsed < _Duration; elapsed += 3)
+        {
+            if (!string.IsNullOrWhiteSpace(Console.ReadLine())) count++;
+        }
+        Console.WriteLine($"You listed {count} items!");
+    }
+}
