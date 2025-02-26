4d6b726f23a5d5e9b8e557948eb426d264801786
Lars Phenix <lars44627@gmail.com>
2025-02-26 12:10:22 -0700

    act1

---
+++ prove/Develop04/Activity.cs
@@ -0,0 +1,20 @@
+class Activity
+{
+    protected int _Duration;
+
+    public void Start()
+    {
+        Console.WriteLine(GetDescription());
+        Console.Write("Enter duration: ");
+        _Duration = int.Parse(Console.ReadLine());
+        Console.WriteLine("Get ready...");
+        Pause(3);
+        Run();
+        Console.WriteLine($"Good job! You did {_Duration} seconds.");
+        Pause(3);
+    }
+
+    protected void Run() {}
+    protected string GetDescription() { return "Activity"; }
+    protected void Pause(int seconds) { for (int i = 0; i < seconds; i++) { Console.Write(".");  } Console.WriteLine(); }
+}
