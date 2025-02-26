d1cb84747cbf7d36221075636bb01c04cf905457
Lars Phenix <lars44627@gmail.com>
2025-02-26 12:26:02 -0700

    ref 1

---
+++ prove/Develop04/ReflectionActivity.cs
@@ -0,0 +1,20 @@
+class ReflectionActivity : Activity
+{
+    string[] _prompts = { "Think of a time you stood up for someone.", "Think of a time you overcame a challenge." };
+    string[] _questions = { "Why was this meaningful?", "How did you feel after?" };
+
+    protected string GetDescription() { return "Reflect on past experiences to recognize your strength."; }
+    protected void Run()
+    {
+        Random rand = new Random();
+        Console.WriteLine(_prompts[rand.Next(_prompts.Length)]);
+        Pause(3);
+        int elapsed = 0;
+        while (elapsed < _Duration)
+        {
+            Console.WriteLine(_questions[rand.Next(_questions.Length)]);
+            Pause(5);
+            elapsed += 5;
+        }
+    }
+}
