97bb62ced71f7f03935fb5035492c7d95c388978
Lars Phenix <lars44627@gmail.com>
2025-03-05 12:35:29 -0700

    bugs1

--- prepare/Learning05/Rectangle.cs
+++ prepare/Learning05/Rectangle.cs
@@ -3,7 +3,7 @@ public class Rectangle : Shape
     private double _width;
     private double _height;
 
-    public Square(string color, double width, double height) : base(color)
+    public Rectangle(string color, double width, double height) : base(color)
     {
         _width = width;
         _height = height;
--- prepare/Learning05/shape.cs
+++ prepare/Learning05/shape.cs
@@ -1,6 +1,6 @@
 using System;
 
-public class Shape
+public abstract class Shape
 {
    protected string color;
 
