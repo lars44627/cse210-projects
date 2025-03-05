ec49bbfaa0084f2bcb67286f9c8e85aa2c9a4cbd
Lars Phenix <lars44627@gmail.com>
2025-03-05 12:30:20 -0700

    prog list

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,8 +4,17 @@ class Program
 {
     static void Main()
     {
-        Square square = new Square("Red", 5);
-        Console.WriteLine("Color: " + square.GetColor());
-        Console.WriteLine("Area: " + square.GetArea());
+       List<Shape> shapes = new List<Shape>
+        {
+            new Square("Red", 5),
+            new Rectangle("Blue", 4, 6),
+            new Circle("Green", 3)
+        };
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"{shape.GetType().Name} Color: {shape.GetColor()}");
+            Console.WriteLine($"{shape.GetType().Name} Area: {shape.GetArea()}");
+        }
     }
 }
