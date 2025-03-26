489075798574b5c776b5294672151beb2fea79f9
Lars Phenix <lars44627@gmail.com>
2025-03-26 17:00:06 -0600

    comments1

---
+++ final/Foundation1/Comment.cs
@@ -0,0 +1,11 @@
+public class Comment
+{
+    public string commenterName;
+    public string text;
+
+    public Comment(string name, string commentText)
+    {
+        commenterName = name;
+        text = commentText;
+    }
+}
